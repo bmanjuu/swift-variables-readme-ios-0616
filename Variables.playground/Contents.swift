a957e6aeeb5bd28f818d13596d0660fa32743d42
bmanjuu <bettyofung@gmail.com>
2016-07-06 09:06:17 -0400

    completee

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,12 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = "Harvey Specter"
+// I really need to start watching GoT!!
+print(favoriteCharacter)
+//NOTE: print also attaches a \n at the end 
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+// ultimateFavoriteCharacter = "Daenerys Targaryen"
